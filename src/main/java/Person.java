c8b906276cfe03de599851cd715ed22927b04a4b
unknown <raje1reddy@gmail.com>
2019-08-20 20:12:52 +0100

    when method reassigns the object references passed to them

---
+++ src/main/java/Person.java
@@ -0,0 +1,16 @@
+
+class Person {
+    private String name;
+    Person(String newName) {
+        name = newName;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String val) {
+        name = val;
+    }
+}
+
